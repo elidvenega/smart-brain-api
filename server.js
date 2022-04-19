2259e9ee76c2bd642f2097905b941b69791de151
elidvenega <venegaelid@gmail.com>
2022-04-18 22:11:36 -0700

    Changes made to server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-  // connect to your own database here:
   client: 'pg',
   connection: {
     host : '127.0.0.1',
@@ -25,7 +24,7 @@ app.use(cors())
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
 app.get('/', (req, res)=> { res.send('its working') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', (req,res) => {signin.handleSignin(req,res,db, bcrypt)})
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
@@ -34,6 +33,11 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 app.listen(process.env.PORT || 3000, ()=> {
   console.log(`app is running on port ${process.env.PORT}`);
 })
+
+
+
+
+
 
 
 
